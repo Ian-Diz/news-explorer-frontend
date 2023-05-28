59a7bdbb5e6bad55e7d88b27201b112a54a5eb1b
Ian <ifd730@gmail.com>
2023-05-27 19:10:52 -0700

    First submission to final project stage 1.1

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -2,6 +2,12 @@ import { getCurrentDate, getLastWeek, processRes } from "./constants";
 
 export const getNews = (input) => {
   return fetch(
-    `https://nomoreparties.co/news/v2/everything?q=${input}&apiKey=1e6911c9a8a542b7a5ca49df266447f5&from=${getLastWeek()}&to=${getCurrentDate}&pageSize=100`
+    `https://nomoreparties.co/news/v2/everything?q=${input}&apiKey=78c44d22767b45f285f2ce167c348ff4&from=${getLastWeek()}&to=${getCurrentDate}&pageSize=100`
+  ).then((res) => processRes(res));
+};
+
+export const getTempArticles = (input) => {
+  return fetch(
+    `https://nomoreparties.co/news/v2/everything?q=${input}&apiKey=78c44d22767b45f285f2ce167c348ff4&pageSize=5`
   ).then((res) => processRes(res));
 };
